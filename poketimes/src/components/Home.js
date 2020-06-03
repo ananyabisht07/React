603e75a4d13d087a690295a8fe1c726c5f2972ce
ananyabisht07 <ananya.bisht@yahoo.in>
2020-06-03 16:52:31 +0530

    Using Axios

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => {
+class Home extends Component{
+
+    state= {
+        posts: [ ]
+    }
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    posts: res.data.slice(0,10)
+                })
+            })
+    }
+
+    render() {
+
+        const { posts } =this.state
+        const postList = posts.length ? (
+            posts.map(post => {
+                return(
+                    <div className=" post card " key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center">No posts to Show.</div>
+        )
+
     return(
         <div className="container">
             <h4 className="center">Home</h4>
-            <p>Hello everybody! This is Home page and I am learning the concept of react router.</p>
+            {postList}
         </div>
-    )
+        )
+    }
 }
 
 export default Home;
--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar= (props) => {
     return(
         <nav className="nav-wrapper red darken-3">
             <div className="container">
-                <a className="brand-logo">Poke'Times</a>
+                <a  href="#" className="brand-logo">Poke'Times</a>
                 <ul className="right">
                     <li><Link to="/">Home</Link></li>
                     <li><NavLink to="/about">About</NavLink></li>
