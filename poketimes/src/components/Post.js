b3e4a3b45a39157d82d89e3c1e2d393230488576
ananyabisht07 <ananya.bisht@yahoo.in>
2020-06-10 19:12:44 +0530

    blog detail page

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import {connect} from 'react-redux';
 
 class Post extends Component {
-    state = {
-       post: null
-    }
-    componentDidMount(){
-        let id =  this.props.match.params.post_id;
-        axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
-            .then(res => {
-                this.setState({
-                    post: res.data
-                })
-               console.log(res)
-           })
-        
-    }
+    // state = {
+    //    post: null
+    // }
+    // componentDidMount(){
+    //     let id =  this.props.match.params.post_id;
+    //     axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+    //         .then(res => {
+    //             this.setState({
+    //                 post: res.data
+    //             })
+    //            console.log(res)
+    //        })
+    // }
     render(){
-         const post =this.state.post ? (
+         const post =this.props.post ? (
              <div className="post">
-                 <h3 className="center">{this.state.post.title}</h3>
-                 <p>{this.state.post.body}</p>
+                 <h3 className="center">{this.props.post.title}</h3>
+                 <p>{this.props.post.body}</p>
              </div>
          ) : (
              <div className="center"><h3>Loading post...</h3></div>
@@ -32,4 +31,12 @@ class Post extends Component {
         );
     }
 }
-export default Post;
+
+const mapStateToProps = (state, ownProps) => {
+    let id = ownProps.match.params.post_id;
+    return {
+        post : state.posts.find(post => post.id === id)
+    }
+}
+
+export default connect(mapStateToProps)(Post);
