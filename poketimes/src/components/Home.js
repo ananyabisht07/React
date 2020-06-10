3c46145e3f0fda2c4a2baabdce922f72d119a924
ananyabisht07 <ananya.bisht@yahoo.in>
2020-06-10 14:42:18 +0530

    Introduction to redux

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import Pokeball from '../pokeball.png';
-
+import { connect } from 'react-redux';
 
 class Home extends Component{
 
-    state= {  //state is a javascript object that is used to store the data locally and contains data state or UI state.
-        posts: [ ]
-    }
+    // state= {  //state is a javascript object that is used to store the data locally and contains data state or UI state.
+    //     posts: [ ]
+    // }
 
-    componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(res => {
-                console.log(res)
-                this.setState({
-                    posts: res.data.slice(0,10)
-                })
-            })
-    }
+    // componentDidMount(){
+    //     axios.get('https://jsonplaceholder.typicode.com/posts')
+    //         .then(res => {
+    //             console.log(res)
+    //             this.setState({
+    //                 posts: res.data.slice(0,10)
+    //             })
+    //         })
+    // }
 
     render() {
 
-        const { posts } =this.state
+        const { posts } =this.props
         const postList = posts.length ? (
             posts.map(post => {
                 return(
@@ -50,4 +49,10 @@ class Home extends Component{
     }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+    return{
+        posts: state.posts
+    }
+}
+
+export default connect(mapStateToProps)(Home);
