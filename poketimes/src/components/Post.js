7bf7b7c066f1d395fe3e85f441300e6d87ffa77c
ananyabisht07 <ananya.bisht@yahoo.in>
2020-06-11 11:29:28 +0530

    Action creator

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import { deletePost } from '../actions/postAction';
 
 class Post extends Component {
     handleClick = () => {
@@ -50,7 +51,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        deletePost: (id) => {dispatch({type: 'DELETE_POST', id: id })}
+        deletePost: (id) => {dispatch(deletePost(id))}
     }
 }
 
