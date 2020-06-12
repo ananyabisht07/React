94ce67cad7131a1e392454792db8eca7cb197a1e
ananyabisht07 <ananya.bisht@yahoo.in>
2020-06-12 11:53:10 +0530

    Using thunk.

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReduder from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReduder);
+const store = createStore(rootReduder, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
