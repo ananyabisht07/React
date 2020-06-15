890166bef1c5c9a9d8219c6392b4923ba4aa8b3e
ananyabisht07 <ananya.bisht@yahoo.in>
2020-06-15 13:28:46 +0530

    Cloud Function Auth Trigger

--- marioplan/functions/index.js
+++ marioplan/functions/index.js
@@ -24,6 +24,20 @@ exports.projectCreated = functions.firestore.document('projects/{projectId}').on
         }   
 
         return createNotification(notification)
+});
 
-    }
-)
+exports.userJoined = functions.auth.user()
+    .onCreate( user => {
+        return admin.firestore().collection('users')
+            .doc(user.uid).get().then(doc => {
+                const newUser = doc.data();
+                const notification = {
+                    content: 'Joined the party',
+                    user: `${newUser.firstName} ${newUser.lastName}`,
+                    time: admin.firestore.FieldValue.serverTimestamp()
+                }
+
+                return createNotification(notification)
+            })
+            
+    })
