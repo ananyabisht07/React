40a74ed8b8e031a458990158f1c2dfaba11ca96a
ananyabisht07 <ananya.bisht@yahoo.in>
2020-06-23 20:43:48 +0530

    custom fonts

--- Review_app/App.js
+++ Review_app/App.js
@@ -1,36 +1,34 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import Home from './screens/home'
+import { AppLoading } from 'expo'
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
-  );
+const getFonts = () => {
+  return Font.loadAsync({
+    'nunito-regular':require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold':require('./assets/fonts/Nunito-Bold.ttf')
+  })
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default function App() {
+  const [fontsLoaded, setFontsLoaded]= useState(false);
+  if (fontsLoaded){
+    return (
+      <Home />
+    );
+  }else {
+    return (
+      <AppLoading 
+        startAsync={ getFonts}
+        onFinish={ () => setFontsLoaded(true)}
+    />
+    )
+  }
+  
+}
+
