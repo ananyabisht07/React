524a03c1491e80c4ff22ebbb12b9e8828f5c0f7a
ananyabisht07 <ananya.bisht@yahoo.in>
2020-07-05 19:33:13 +0530

    Signing Users Out

--- Firebase-Auth/scripts/auth.js
+++ Firebase-Auth/scripts/auth.js
@@ -16,3 +16,12 @@ signupForm.addEventListener('submit', (e) => {
     signupForm.reset();
     });
 });
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signed out');
+  })
+});
