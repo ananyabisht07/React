31e9da82771eae39878ba84a4f76bf609585a865
ananyabisht07 <ananya.bisht@yahoo.in>
2020-07-05 16:00:04 +0530

    Signing up users

---
+++ Firebase-Auth/scripts/auth.js
@@ -0,0 +1,18 @@
+// sign up
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+    //console.log(email, password)
+    // sign up the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user);
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+    });
+});
