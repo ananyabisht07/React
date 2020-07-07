127150c333447071f485fcb042448c121eac2187
ananyabisht07 <ananya.bisht@yahoo.in>
2020-07-07 13:26:49 +0530

    Catching Auth Errors

--- Firebase-Auth/scripts/auth.js
+++ Firebase-Auth/scripts/auth.js
@@ -66,6 +66,9 @@ const signupForm = document.querySelector('#signup-form');
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset();
+        signupForm.querySelector('.error').innerHTML = '';
+    }).catch(err => {
+        signupForm.querySelector('.error').innerHTML = err.message;
     })
 });
 
@@ -91,6 +94,9 @@ loginForm.addEventListener('submit', (e) => {
     const modal = document.querySelector('#modal-login');
     M.Modal.getInstance(modal).close();
     loginForm.reset();
-    });
+    loginForm.querySelector('.error').innerHTML = '';
+    }).catch(err => {
+        loginForm.querySelector('.error').innerHTML = err.message;
+    })
 
 });
